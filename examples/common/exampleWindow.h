987df4010154c7b5efa173708d1220c675a105d3
SlinkierElm5611 <stefan.balta.sb@gmail.com>
2026-09-21 23:58:13 -0400

    Addressed windows build error

--- examples/common/exampleWindow.h
+++ examples/common/exampleWindow.h
@@ -24,6 +24,9 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #if defined(_WIN32)
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #define GLFW_EXPOSE_NATIVE_WIN32
 #elif defined(__APPLE__)
 #define GLFW_EXPOSE_NATIVE_COCOA
